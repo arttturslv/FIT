4be90a6a2718686d7126e42c4c4d028838ed444d
arttturslv <arttturslv@gmail.com>
2024-07-06 17:17:04 -0300

    first commit: added React + TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],  theme: {
+    extend: {},
+    colors: {
+      'yellow': '#D28C42',
+      'peach': '#F1D2AE',
+      'navy': '#272A31',
+      'black': '#141819'
+    }
+  },
+  plugins: [],
+}
+
